7b96e4fb0cd3034d29bb86280ceb83c63b22064c
Liu Minjie <liuminjie@loongson.cn>
2022-07-11 19:00:04 +0800

    feat: basic functionality

--- fe.js
+++ fe.js
@@ -1,3 +1,29 @@
-(function() {
-    // TODO:
-})();
+document.addEventListener("mouseover", mouseOver);
+document.addEventListener("mouseout", mouseOut);
+document.addEventListener("click", click);
+
+function mouseOver(event) {
+    if (!document.fullscreenElement) {
+        event.target.style.outline = "3px rgba(134,206,203, 0.8) solid";
+    }
+}
+
+function mouseOut(event) {
+    if (!document.fullscreenElement) {
+        event.target.style.outline = "";
+    }
+}
+
+function click(event) {
+    openFullscreen(event.target);
+    event.target.style.outline = "";
+    // TODO: backgroundColor
+    event.target.style.backgroundColor = 'white';
+}
+
+function openFullscreen(elem) {
+    /* only fullscreen when not  */
+    if (!document.fullscreenElement) {
+        elem.requestFullscreen();
+    }
+}
